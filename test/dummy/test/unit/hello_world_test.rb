3e74ccb097d760966abdf69bb31b9aee4c572257
Eddie Gonzales <egonzales@pureplay.com>
2011-06-03 22:30:44 -0700

    removing test

--- test/dummy/test/unit/hello_world_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class HelloWorldTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    puts 'hello world'
-    assert true
-  end
-end
