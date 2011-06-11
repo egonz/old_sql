caf0a469d9831760987fbf54ff013400a93a6fa0
Eddie Gonzales <egonzales@pureplay.com>
2011-06-10 20:33:45 -0700

    added a new generator to copy assets (old_sql.css) and the layout view, to the app instatallation, which will allow the user to further customize old_sql.

--- lib/generators/old_sql/old_sql_generator.rb
+++ lib/generators/old_sql/old_sql_generator.rb
@@ -15,6 +15,11 @@ Devise, but want use another model than the default 'user' you can provide the
 custom model name as an argument:
 
   rails g old_sql:install member
+  
+To copy file assets, old_sql.css and the layout for the report view, to your 
+installation, execute:
+
+  rails g old_sql:copy_assets
 
 "
     end
